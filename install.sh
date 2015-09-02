3868dae9334c113ae693ca5f20c466d605c76cb0
notjrbauer <notjrbauer@gmail.com>
2015-09-02 15:01:07 -0700

    add sleep to buy enough time to compile

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ sudo apt-get -y install linux-image-extra-$(uname -r)
 # Add Docker PPA and install latest version
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
 sudo sh -c "echo deb https://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
+sleep 3
 sudo apt-get update
 sudo apt-get install lxc-docker -y
 
